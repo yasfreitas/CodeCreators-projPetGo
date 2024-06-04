1e33722507334269f0ab119e3598d7ad40532535
yasfreitas <yasmin.freitas1707@gmail.com>
2024-06-04 12:15:10 -0300

    Projeto PetGo

---
+++ src/main/java/com/petGo/repository/ConsultaRepository.java
@@ -0,0 +1,9 @@
+package com.petGo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.petGo.entities.Consulta;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long>{
+
+}
